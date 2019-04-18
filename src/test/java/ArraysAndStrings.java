fda30f50701c0b936e5d3f033b4eb6ce3ba2b81f
S. Matthew English <s.matthew.english@gmail.com>
2019-04-18 14:52:05 -0400

    x

--- src/test/java/ArraysAndStrings.java
+++ src/test/java/ArraysAndStrings.java
@@ -316,6 +316,45 @@ public class ArraysAndStrings {
         int bitVector = createBitVector(phrase);
         return checkAtMostOneBitSet(bitVector);
     }
+
+    /**
+     * There are three types of edits that can be performed on strings: insert a character,
+     * remove a character, or replace a character. Given two strings, write a function to
+     * check if they are one edit (or zero edits) away.
+     *
+     * EXAMPLE
+     *
+     * "pale",  "ple"  --> true
+     * "pales", "pale" --> true
+     * "pale",  "bale" --> true
+     * "pale",  "bae"  --> false
+     *
+     * There's a "brute force" algorithm to do this. We could check all possible strings that
+     * are one edit away by testing the removal of each character (and comparing), testing
+     * the replacement of each character (and comparing), and then testing the insertion
+     * of each possible character (and comparing).
+     *
+     * That would be too slow, so let's not bother with implementing it.
+     *
+     * This is one of those problems where it's helpful to think about the "meaning" of each
+     * of these operations. What does it mean for two strings to be one insertion, replacement,
+     * or removal away from each other?
+     *
+     * REPLACEMENT: Consider two strings such as "bale" and "pale", that are one replacement away.
+     * Yes, that does mean that you could replace a character in "bale" to make "pale". But more
+     * precisely, it means that they are different in only one place.
+     *
+     * INSERTION: The strings "apple" and "aple" are one insertion away. This means that if you compared
+     * the strings, they would be idential -- except for a shift at some point ion the strings.
+     *
+     * REMOVAL: The strings "apple" and "aple" are also one removal away, since removal is just the inverse
+     * of insertion.
+     */
+
+    
+
+
+
 }
 
 
