239e234e774d6fba343ab4d76840377f3f07e03d
S. Matthew English <s.matthew.english@gmail.com>
2019-04-20 03:07:36 -0400

    rotate

--- src/test/java/ArraysAndStrings.java
+++ src/test/java/ArraysAndStrings.java
@@ -584,7 +584,114 @@ public class ArraysAndStrings {
         System.out.println("r0: " + r0);
     }
 
-    
+    /**
+     * Given an image represented by an NxN matrix, where each pixel in the image is
+     * represented by an integer, write a method to rotate the image by 90 degrees.
+     * Can you do it in place?
+     *
+     * Because we're rotating the matrix by 90 degrees, the easiest way to do this is
+     * to implement the rotation in layers. We perform a circular rotation on each
+     * layer, moving the top edge to the right edge, the right edge to the bottom
+     * edge, the bottom edge to the left edge, and the left edge to the top edge.
+     *
+     * How do we perform this four-way edge swap?
+     *
+     * One option is to copy the top edge to an array, and then move the left to the top,
+     * the bottom to the left, and so on. This requires O(N) memory, which is actually
+     * unnecessary.
+     *
+     * A better way to do this is to implement the swap index by index. In this case, we
+     * do the following:
+     *
+     * for i = 0 to n
+     *     temp = top[i]
+     *     top[i] = left[i]
+     *     left[i] = bottom[i]
+     *     bottom[i] = right[i]
+     *     right[i] = temp
+     *
+     * We perform such a swap on each layer, starting from the outermost layer and working
+     * our way inwards. (Alternatively, we could start from the inner layer and work outwards)
+     */
+    private boolean rotate(int[][] matrix) {
+        if(matrix.length == 0 || matrix.length != matrix[0].length) {
+            return false;
+        }
+
+        int n = matrix.length;
+        for(int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for(int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void rotateMatrix() {
+
+        int[] a0 = new int[]{0, 0, 0, 0};
+        int[] a1 = new int[]{1, 1, 1, 1};
+        int[] a2 = new int[]{2, 2, 2, 2};
+        int[] a3 = new int[]{3, 3, 3, 3};
+
+        int[][] matrix = new int[][]{a0, a1, a2, a3};
+        System.out.println(Arrays.deepToString(matrix).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+
+        //boolean r0 = rotate(matrix);
+        //System.out.println("r0: " + r0);
+        System.out.println();
+
+        int[][] matrix0 = rotateX(matrix);
+        System.out.println(Arrays.deepToString(matrix0).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+    }
+
+
+    /**
+     * This algorithm is O(N^2), which is the best we can do since any algorithm must touch all N^2 elements.
+     */
+    private int[][] rotateX(int[][] matrix) {
+
+        int n = matrix.length;
+        for(int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+
+            for(int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+        return matrix;
+    }
 
 }
 
